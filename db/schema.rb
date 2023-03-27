3efa24e957c56a60714e9ddec21a665ebb27e817
enankerv <enankerv@andrew.cmu.edu>
2023-03-26 22:09:16 -0400

    Fixed starter code

--- db/schema.rb
+++ db/schema.rb
@@ -2,22 +2,21 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217221809) do
-
+ActiveRecord::Schema[7.0].define(version: 2014_02_17_221809) do
   create_table "children", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.boolean "active", default: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "chores", force: :cascade do |t|
@@ -25,16 +24,16 @@ ActiveRecord::Schema.define(version: 20140217221809) do
     t.integer "task_id"
     t.date "due_on"
     t.boolean "completed", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.integer "points"
     t.boolean "active", default: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
